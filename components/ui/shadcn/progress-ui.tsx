cbc2f08ba8645f7585cfd89bc7e1faeb9b4b57fe
Matt Weichel <maweiche@gmail.com>
2024-10-02 17:25:49 -0700

    fixed overlay issue with login

--- components/ui/shadcn/progress-ui.tsx
+++ components/ui/shadcn/progress-ui.tsx
@@ -12,7 +12,7 @@ const Progress = React.forwardRef<
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      "relative h-2 w-full overflow-hidden rounded-full bg-black/20 dark:bg-white/20",
+      "relative h-2 w-full overflow-hidden rounded-full bg-black/20 dark:bg-white/20 border border-slate-grey-200/10 dark:border-white/10",
       className
     )}
     {...props}
